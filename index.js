f98a63e24ade33e55aa8b6eeca0db711f4ce422b
aldoprogrammer <aldobesma@gmail.com>
2024-03-03 11:33:52 +0700

    'update'

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 const cors = require('cors')
+require('dotenv').config();
 
 // middleware
 app.use(cors())
@@ -10,8 +11,7 @@ app.use(express.json());
 // mongodb configuration
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = "mongodb+srv://aldobesma:76oFinUDdd9IJYYz@cluster0.nientyt.mongodb.net/?retryWrites=true&w=majority";
-
+const uri = process.env.MONGODB_URI;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
